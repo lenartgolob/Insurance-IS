b4e2dc32da79b59ed031995217f89d0d15e13bf9
Lenart GOlob <lenart.golob17@gmail.com>
2021-11-26 21:44:45 +0100

    Data annotations

--- web/Models/Insured.cs
+++ web/Models/Insured.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace web.Models
 {
     public class Insured
     {
         public int ID { get; set; }
+        [StringLength(50)]
         public string LastName { get; set; }
+        [StringLength(50)]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
 
 
